2e69bf7ef019659aec3eb411f6cdb20676fd885d
antjamespadgett <padgettanthonyj@gmail.com>
2021-12-14 15:12:56 -0600

    completed exercise 1

---
+++ Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+
+        public Car ( string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            
+        }
+        //Constructors
+        public Car()// - a special member method that has the same name as its class
+        {
+
+        }
+
+        //fields
+        //Properties
+        //Methods
+
+        //Create a Make property of typr=r string that is public
+        public string Make { get; set; }// get = read set = write
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+
+
+    }
+   
+
+    
+
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();// Constructor- special member method.
+            myCar.Make = "Yugo";
+            myCar.Model = "Neon";
+            myCar.Year = 1988;
+
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "Millenia",
+                Year = 2002
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2006);
+
+            var carlist = new List<Car>() { myCar, mazda, chevy };
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}  {vehicle.Model} {vehicle.Year}");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
